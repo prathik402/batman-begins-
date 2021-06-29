46abd3011a51ba989480e28c11fe061d7370eee6
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-06-29 18:32:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 const Engine= Matter.Engine
 const Bodies = Matter.Bodies
 const World = Matter.World
-const 
-var maxDrops = 100;
+var engine,world
+var maxDrops 
 
 function preload(){
     
@@ -10,6 +10,19 @@ function preload(){
 
 function setup(){
 var canvas = createCanvas(1200,400);
+
+
+if(frameCount % 150 === 0){ for(var i=0; i<maxDrops; i++){ drops.push(new createDrop(random(0,400), random(0,400))); } }
+
+
+
+
+
+
+
+
+
+
 engine = Engine.create();
 world = engine.world;
 
@@ -18,6 +31,11 @@ world = engine.world;
 }
 
 function draw(){
-  
+background("black")
+for(var i = 0; i<maxDrops; i++){ drops[i].showDrop(); drops[i].updateY() }
+
+
+
+drawSprites();    
 }   
 
